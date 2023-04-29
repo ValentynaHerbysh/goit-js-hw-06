a985d74239f74384a4a25efb5d4d4874cc53a6f4
ValentynaHerbysh <valentinagerbish@ukr.net>
2023-04-29 21:21:57 +0300

    HW_JS_6

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const color = document.querySelector('.color'); 
+const changeColorEl = document.querySelector('.change-color');
+
+changeColorEl.addEventListener('click', changeColor);
+
+function changeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+
+  
+}
+color.textContent = document.body.getAttribute('backgroundColor');
